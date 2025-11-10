97ea9d4b84c4abc4e1f52cdc32e5533e5d949418
mpnogaj <mpnogaj@gmail.com>
2025-11-10 19:01:16 +0100

    Linting multiple files

--- lint_worker.js
+++ lint_worker.js
@@ -70,91 +70,41 @@ var print_fn = function (s) {
   logToMain(s);
 }
 
-self.onmessage = (e) => {
-  logToMain("Worker: Received Verilog code from main page.");
-  const verilogCode = e.data.code;
-  const VIRTUAL_FILE_NAME = 'lint_target.v';
-
-  // 7. Write the Verilog code to the virtual file system.
-  try {
-    Module.FS.writeFile(VIRTUAL_FILE_NAME, verilogCode);
-    logToMain(`Worker: Wrote virtual file: /${VIRTUAL_FILE_NAME}`);
-  } catch (err) {
-    logToMain(`Worker: Error writing to VFS: ${err}`);
-    return;
-  }
-
-  // 8. Prepare the command-line arguments.
-  const args = [
-    '--Wall',         // Enable all common warnings [3, 4]
-    VIRTUAL_FILE_NAME // The file to lint
-  ];
-
-  // 9. Execute Verilator.
-  logToMain(`Worker: Executing: ${args.join(' ')}\n------------------`);
-  try {
-    // This runs the C++ main() function [5]
-    Module.callMain(args);
-  } catch (e) {
-    logToMain(`Worker: Runtime exception (this is often normal): ${e}`);
-  }
-  logToMain("------------------\nWorker: Linting complete.");
-  self.postMessage({ type: 'done' });
-};
-
-try {
-	loadVerilator().then(() => {
-		logToMain("Worker: Loaded verilator_bin.js successfully.");
-	{
-			let verilator_mod = self.verilator_bin({
-				instantiateWasm: moduleInstFn(),
-				noInitialRun: true,
-				noExitRuntime: true,
-				print: print_fn,
-				printErr: (s) => {console.error(s); },
-				wasmMemory: getWASMMemory(),
-			});
-			const args = ['--version'];
-			const mainFn = verilator_mod.callMain || verilator_mod.run;
-			mainFn(args);
-		}
-
-		{
-			let stderr_capture = [];
-			let verilator_mod = self.verilator_bin({
-				instantiateWasm: moduleInstFn(),
-				noInitialRun: true,
-				noExitRuntime: true,
-				print: print_fn,
-				printErr: (s) => { stderr_capture.push(s); },
-				wasmMemory: getWASMMemory(),
-			});
-			const DUMMY_FILE_NAME = 'test.sv';
-const DUMMY_FILE_CONTENT = `
-module test(input logic test, input logic unused, output logic out);
-	assign out = test;
-endmodule
-
-module top(input logic a);
-	test t1(.test(a), .unused(_1),  .out(_2));
-endmodule
-`;
-
-			verilator_mod.FS.writeFile(DUMMY_FILE_NAME, DUMMY_FILE_CONTENT);
-			const args = ['--lint-only', '--Wall', '-Wno-DECLFILENAME', '-Wno-UNOPT', '-Wno-UNOPTFLAT', DUMMY_FILE_NAME];
-			const mainFn = verilator_mod.callMain || verilator_mod.run;
-			mainFn(args); // execution is synchronous
-			if (stderr_capture.length > 1) {
-				stderr_capture.pop();
-				for (let line of stderr_capture) {
-					logToMain(line);
-				}
-			}
-		}
-
-		console.log('Run called?');
+function lintVerilogCode(files) {
+	let stderr_capture = [];
+	let verilator_mod = self.verilator_bin({
+		instantiateWasm: moduleInstFn(),
+		noInitialRun: true,
+		noExitRuntime: true,
+		print: print_fn,
+		printErr: (s) => { stderr_capture.push(s); },
+		wasmMemory: getWASMMemory(),
 	});
 
-} catch (e) {
-  logToMain(`Worker: CRITICAL ERROR: Failed to load verilator_bin.js. ${e}`);
+	const filenames = Object.keys(files);
+
+	filenames.forEach((filename) => {
+		verilator_mod.FS.writeFile(filename, files[filename]);
+	});
+
+	const args = ['--lint-only', '--Wall', '-Wno-DECLFILENAME', '-Wno-UNOPT', '-Wno-UNOPTFLAT'].concat(filenames);
+	const mainFn = verilator_mod.callMain || verilator_mod.run;
+	mainFn(args); // execution is synchronous
+	if (stderr_capture.length > 1) {
+		stderr_capture.pop();
+		for (let line of stderr_capture) {
+			logToMain(line);
+		}
+	}
 }
+
+self.onmessage = async (e) => {
+	if (e.data.type === 'lint') {
+		await loadVerilator();
+		logToMain("Worker: Starting linting process...");
+		const files = e.data.files;
+
+		lintVerilogCode(files);
+		self.postMessage({ type: 'done' });
+	}
+};
